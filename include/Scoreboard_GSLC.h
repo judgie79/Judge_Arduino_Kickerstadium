1c9ad445fb0620870a5cbe66dae5d1344579f9f9
Michael Richter <mrichter@leondrino.com>
2023-02-16 10:11:03 +0100

    add ui dummy for scoreboard

---
+++ include/Scoreboard_GSLC.h
@@ -0,0 +1,161 @@
+//<File !Start!>
+// FILE: [Scoreboard_GSLC.h]
+// Created by GUIslice Builder version: [0.17.b19]
+//
+// GUIslice Builder Generated GUI Framework File
+//
+// For the latest guides, updates and support view:
+// https://github.com/ImpulseAdventure/GUIslice
+//
+//<File !End!>
+
+#ifndef _GUISLICE_GEN_H
+#define _GUISLICE_GEN_H
+
+// ------------------------------------------------
+// Headers to include
+// ------------------------------------------------
+#include "GUIslice.h"
+#include "GUIslice_drv.h"
+
+// Include any extended elements
+//<Includes !Start!>
+//<Includes !End!>
+
+// ------------------------------------------------
+// Headers and Defines for fonts
+// Note that font files are located within the Adafruit-GFX library folder:
+// ------------------------------------------------
+//<Fonts !Start!>
+#if defined(DRV_DISP_TFT_ESPI)
+  #error E_PROJECT_OPTIONS tab->Graphics Library should be TFT_eSPI
+#endif
+#include <Adafruit_GFX.h>
+//<Fonts !End!>
+
+// ------------------------------------------------
+// Defines for resources
+// ------------------------------------------------
+//<Resources !Start!>
+//<Resources !End!>
+
+// ------------------------------------------------
+// Enumerations for pages, elements, fonts, images
+// ------------------------------------------------
+//<Enum !Start!>
+enum {E_PG_MAIN};
+enum {E_ELEM_TEXT4,MATCH_TIME,SCORE_BLUE,SCORE_RED};
+// Must use separate enum for fonts with MAX_FONT at end to use gslc_FontSet.
+enum {E_BUILTIN20X32,MAX_FONT};
+//<Enum !End!>
+
+// ------------------------------------------------
+// Instantiate the GUI
+// ------------------------------------------------
+
+// ------------------------------------------------
+// Define the maximum number of elements and pages
+// ------------------------------------------------
+//<ElementDefines !Start!>
+#define MAX_PAGE                1
+
+#define MAX_ELEM_PG_MAIN 4 // # Elems total on page
+#define MAX_ELEM_PG_MAIN_RAM MAX_ELEM_PG_MAIN // # Elems in RAM
+//<ElementDefines !End!>
+
+// ------------------------------------------------
+// Create element storage
+// ------------------------------------------------
+gslc_tsGui                      m_gui;
+gslc_tsDriver                   m_drv;
+gslc_tsFont                     m_asFont[MAX_FONT];
+gslc_tsPage                     m_asPage[MAX_PAGE];
+
+//<GUI_Extra_Elements !Start!>
+gslc_tsElem                     m_asPage1Elem[MAX_ELEM_PG_MAIN_RAM];
+gslc_tsElemRef                  m_asPage1ElemRef[MAX_ELEM_PG_MAIN];
+
+#define MAX_STR                 100
+
+//<GUI_Extra_Elements !End!>
+
+// ------------------------------------------------
+// Program Globals
+// ------------------------------------------------
+
+// Element References for direct access
+//<Extern_References !Start!>
+//<Extern_References !End!>
+
+// Define debug message function
+static int16_t DebugOut(char ch);
+
+// ------------------------------------------------
+// Callback Methods
+// ------------------------------------------------
+bool CbBtnCommon(void* pvGui,void *pvElemRef,gslc_teTouch eTouch,int16_t nX,int16_t nY);
+bool CbCheckbox(void* pvGui, void* pvElemRef, int16_t nSelId, bool bState);
+bool CbDrawScanner(void* pvGui,void* pvElemRef,gslc_teRedrawType eRedraw);
+bool CbKeypad(void* pvGui, void *pvElemRef, int16_t nState, void* pvData);
+bool CbListbox(void* pvGui, void* pvElemRef, int16_t nSelId);
+bool CbSlidePos(void* pvGui,void* pvElemRef,int16_t nPos);
+bool CbSpinner(void* pvGui, void *pvElemRef, int16_t nState, void* pvData);
+bool CbTickScanner(void* pvGui,void* pvScope);
+
+// ------------------------------------------------
+// Create page elements
+// ------------------------------------------------
+void InitGUIslice_gen()
+{
+  gslc_tsElemRef* pElemRef = NULL;
+
+  if (!gslc_Init(&m_gui,&m_drv,m_asPage,MAX_PAGE,m_asFont,MAX_FONT)) { return; }
+
+  // ------------------------------------------------
+  // Load Fonts
+  // ------------------------------------------------
+//<Load_Fonts !Start!>
+    if (!gslc_FontSet(&m_gui,E_BUILTIN20X32,GSLC_FONTREF_PTR,NULL,4)) { return; }
+//<Load_Fonts !End!>
+
+//<InitGUI !Start!>
+  gslc_PageAdd(&m_gui,E_PG_MAIN,m_asPage1Elem,MAX_ELEM_PG_MAIN_RAM,m_asPage1ElemRef,MAX_ELEM_PG_MAIN);
+
+  // NOTE: The current page defaults to the first page added. Here we explicitly
+  //       ensure that the main page is the correct page no matter the add order.
+  gslc_SetPageCur(&m_gui,E_PG_MAIN);
+  
+  // Set Background to a flat color
+  gslc_SetBkgndColor(&m_gui,GSLC_COL_BLACK);
+
+  // -----------------------------------
+  // PAGE: E_PG_MAIN
+  
+  
+  // Create SCORE_RED text label
+  pElemRef = gslc_ElemCreateTxt(&m_gui,SCORE_RED,E_PG_MAIN,(gslc_tsRect){234,104,24,32},
+    (char*)"0",0,E_BUILTIN20X32);
+  gslc_ElemSetFillEn(&m_gui,pElemRef,false);
+  
+  // Create SCORE_BLUE text label
+  pElemRef = gslc_ElemCreateTxt(&m_gui,SCORE_BLUE,E_PG_MAIN,(gslc_tsRect){60,104,24,32},
+    (char*)"0",0,E_BUILTIN20X32);
+  gslc_ElemSetFillEn(&m_gui,pElemRef,false);
+  
+  // Create E_ELEM_TEXT4 text label
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_TEXT4,E_PG_MAIN,(gslc_tsRect){148,104,24,32},
+    (char*)":",0,E_BUILTIN20X32);
+  gslc_ElemSetFillEn(&m_gui,pElemRef,false);
+  
+  // Create MATCH_TIME text label
+  pElemRef = gslc_ElemCreateTxt(&m_gui,MATCH_TIME,E_PG_MAIN,(gslc_tsRect){148,10,24,32},
+    (char*)"0",0,E_BUILTIN20X32);
+  gslc_ElemSetFillEn(&m_gui,pElemRef,false);
+//<InitGUI !End!>
+
+//<Startup !Start!>
+//<Startup !End!>
+
+}
+
+#endif // end _GUISLICE_GEN_H
